8f9310b54b92359813299089e3d65eab817a6f12
Christian Heider Nielsen <mrnaah@gmail.com>
2019-05-01 11:25:11 +0200

    Version

--- WargPackage.py
+++ WargPackage.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages
 
-from version import get_version
+from warg.version import get_version
 
 
 class WargPackage:
--- warg/__init__.py
+++ warg/__init__.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+from warg.version import get_version
 
 __author__ = "cnheider"
+__version__ = get_version()
+__doc__ = """
+Created on 27/04/2019
+
+@author: cnheider
+"""
 
 from .named_ordered_dictionary import *
 from .arguments import *
