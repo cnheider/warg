17e4891899fae349b1a1e7bc0a0f2b5b547344ec
cnheider <mrnaah@gmail.com>
2019-06-14 09:57:28 +0200

    0.2.1

--- warg/mixins.py
+++ warg/mixins.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import numpy
+import torch
+
+from agent.utilities import to_tensor
 
 __author__ = "cnheider"
 __doc__ = ""
@@ -20,5 +24,20 @@ class IterKeysMixin(object):
 class IterValuesMixin(object):
     def __iter__(self):
         a = self.__dict__
-        for value in self.__dict__.values():
+        for value in a.values():
             yield value
+
+
+class IndexDictTuplesMixin(object):
+    def __getitem__(self, item):
+        a = self.__dict__
+        b = numpy.array(list(a.values()))
+        return b[item]
+
+
+class TensoriseMixin(object):
+    device = "cpu"
+    dtype = torch.float
+
+    def __setattr__(self, key, value):
+        super().__setattr__(key, to_tensor(value, dtype=self.dtype, device=self.device))
