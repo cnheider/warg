d08043986a1f1464f2d733b35eb5a52bbac911c9
Christian Heider Nielsen <christian.heider@alexandra.dk>
2022-04-08 13:40:36 +0200

    bump

--- tests/req_unit_test.py
+++ tests/req_unit_test.py
@@ -2,6 +2,7 @@
 
 import unittest
 from pathlib import Path
+import pytest
 
 import pkg_resources
 
@@ -20,6 +21,7 @@ class TestRequirements(unittest.TestCase):
             with self.subTest(requirement=requirement):
                 pkg_resources.require(requirement)
 
+    @pytest.mark.xfail(strict=False)  # DO not successfully parse recursing of reqs using -r
     def test_extra_requirements(self):
         """Test that each required package is available."""
         if _EXTRA_REQUIREMENTS_PATH.exists():
