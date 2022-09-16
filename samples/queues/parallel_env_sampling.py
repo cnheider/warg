79216e3add4b79982e1ab49090f1bd0366d23942
Christian Heider Nielsen <christian.heider@alexandra.dk>
2022-09-16 15:27:37 +0200

    bump

--- samples/queues/parallel_env_sampling.py
+++ samples/queues/parallel_env_sampling.py
@@ -42,35 +42,35 @@ def plus(a, b):
     return a + b
 
 
-def test():
-    NUMBER_OF_PROCESSES = 4
-    TASKS1 = [(mul, (i, 7)) for i in range(20)]
-    TASKS2 = [(plus, (i, 8)) for i in range(10)]
+def stest():
+    number_of_processes = 4
+    tasks1 = [(mul, (i, 7)) for i in range(20)]
+    tasks2 = [(plus, (i, 8)) for i in range(10)]
 
     # Create queues
     task_queue = Queue()
     done_queue = Queue()
 
-    for task in TASKS1:  # Submit tasks
+    for task in tasks1:  # Submit tasks
         task_queue.put(task)
 
-    for i in range(NUMBER_OF_PROCESSES):  # Start worker processes
+    for i in range(number_of_processes):  # Start worker processes
         Process(target=worker, args=(task_queue, done_queue)).start()
 
     print("Unordered results:")
-    for i in range(len(TASKS1)):  # Get and print results
+    for i in range(len(tasks1)):  # Get and print results
         print("\t", done_queue.get())
 
-    for task in TASKS2:  # Add more tasks using `put()`
+    for task in tasks2:  # Add more tasks using `put()`
         task_queue.put(task)
 
-    for i in range(len(TASKS2)):  # Get and print some more results
+    for i in range(len(tasks2)):  # Get and print some more results
         print("\t", done_queue.get())
 
-    for i in range(NUMBER_OF_PROCESSES):  # Tell child processes to stop
+    for i in range(number_of_processes):  # Tell child processes to stop
         task_queue.put("STOP")
 
 
 if __name__ == "__main__":
     freeze_support()
-    test()
+    stest()
